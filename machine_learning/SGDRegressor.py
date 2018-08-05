ee6110131d1d28203dd3b91b0f0abd0d2519f6ba
Anthony Buchholz <anthony.buchholz@gmail.com>
2018-08-04 22:56:39 -0500

    SGD Regressor doesn't seem to want to vary much

--- machine_learning/SGDRegressor.py
+++ machine_learning/SGDRegressor.py
@@ -1,18 +1,24 @@
 import multiprocessing
-import numpy as np
 from sklearn import linear_model
 
 from get_data_sets import get_points_data_set, get_x_points_data_set, get_y_points_data_set
 
 if __name__ == '__main__':
-    data_set = get_points_data_set(1128, 7, multiprocessing.Lock())
-    print(data_set)
-    sgd_regressor = linear_model.SGDRegressor()
-    training_x_points = get_x_points_data_set(data_set)[0]
-    testing_x_points = get_x_points_data_set(data_set)[1]
-    training_y_points = get_y_points_data_set(data_set)[0]
-    testing_y_points = get_y_points_data_set(data_set)[1]
-    print(training_x_points)
-    print(training_y_points)
+    data_set = get_points_data_set(554, 14, multiprocessing.Lock())
+    # print(data_set)
+    sgd_regressor = linear_model.SGDRegressor(loss="epsilon_insensitive", tol=.3, eta0=.0001, power_t=.15)
+    x_points_data_set = get_x_points_data_set(data_set)
+    y_points_data_set = get_y_points_data_set(data_set)
+    # print("x points: {}".format(x_points_data_set))
+    # print("y points: {}".format(y_points_data_set))
+    training_x_points = x_points_data_set[0]
+    testing_x_points = x_points_data_set[1]
+    training_y_points = y_points_data_set[0]
+    testing_y_points = y_points_data_set[1]
+    # print(training_x_points)
+    # print(training_y_points)
     sgd_regressor.fit(training_x_points, training_y_points)
-    print(sgd_regressor.score(training_x_points, training_y_points))
+    print("Predicting: {}".format([int(round(x)) for x in sgd_regressor.predict(testing_x_points)]))
+    # print("Tried: {}".format(testing_x_points))
+    print("Expected:   {}".format([x for x in testing_y_points]))
+    print(sgd_regressor.score(testing_x_points, testing_y_points))
--- machine_learning/get_data_sets.py
+++ machine_learning/get_data_sets.py
@@ -4,11 +4,15 @@ import numpy as np
 from database.in_database import get_days_for_item
 
 
+def flatten_list(input_list: list) -> list:
+    return [item for sub_list in input_list for item in sub_list]
+
+
 def get_points_data_set(item_id: int, data_range: int, session_lock: multiprocessing.Lock) -> list:
     points = [(x[0]//86400000, x[1]) for x in get_days_for_item(item_id, session_lock)]
-    return [get_points_for_day(points, point, data_range)[0] for point in points if
+    return [get_points_for_day(points, point, data_range) for point in points if
             any(point[0] < inner_point[0] <= point[0] + data_range for inner_point in points)
-            and len(get_points_for_day(points, point, data_range)) == 7]
+            and len(get_points_for_day(points, point, data_range)) == data_range + 1]
 
 
 def get_x_points_data_set(data_set: list) -> np.array:
@@ -16,8 +20,10 @@ def get_x_points_data_set(data_set: list) -> np.array:
     data_set_size = len(data_set)
     training_data_set = data_set[:round(data_set_size * .8)]
     test_data_set = data_set[round(data_set_size * .8):]
-    return [np.array([data[:data_points_size-1] for data in training_data_set]),
-            np.array([data[:data_points_size-1] for data in test_data_set])]
+    training_data_set_holder = training_data_set[:]
+    testing_data_set_holder = test_data_set[:]
+    return [[data[1:] for data in training_data_set_holder],
+            [data[1:] for data in testing_data_set_holder]]
 
 
 def get_y_points_data_set(data_set: list) -> np.array:
@@ -25,17 +31,22 @@ def get_y_points_data_set(data_set: list) -> np.array:
     data_set_size = len(data_set)
     training_data_set = data_set[:round(data_set_size * .8)]
     test_data_set = data_set[round(data_set_size * .8):]
-    return [np.array([data[data_points_size - 1:][0] for data in training_data_set]),
-            np.array([data[data_points_size - 1:][0] for data in test_data_set])]
+    return [np.array([data[0] for data in training_data_set]),
+            np.array([data[0] for data in test_data_set])]
 
 
 def get_points_for_day(points: list, end_point: tuple, data_range: int) -> list:
-    return [x for x in points if end_point[0] < x[0] <= end_point[0] + data_range]
+    return [x[1] for x in points if end_point[0] <= x[0] <= end_point[0] + data_range]
 
 
 if __name__ == '__main__':
     lock = multiprocessing.Lock()
-    a_list = get_points_data_set(554, 7, lock)
-    print(a_list[0])
-    print(get_x_points_data_set(a_list)[0])
-    print(get_y_points_data_set(a_list)[0])
+    points = [(x[0] // 86400000, x[1]) for x in get_days_for_item(554, lock)]
+    data_set = get_points_data_set(554, 7, lock)
+    print(points)
+    print(get_points_for_day(points, (1520726400000 // 86400000, 123), 7))
+    print(data_set)
+    x_points = get_x_points_data_set(data_set)
+    y_points = get_y_points_data_set(data_set)
+    print(x_points[0])
+    print(y_points[0])
